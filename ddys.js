1dea1f8393fddb5c263fe41e4dcfb080278d71f4
Regulus <zjjscwt@gmail.com>
2025-08-07 19:36:52 +0800

    Update ddys.js

--- ddys.js
+++ ddys.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         低端影视DDYS优化
 // @namespace    https://github.com/s0urcelab/userscripts
-// @version      1.1.1
+// @version      1.2.0
 // @description  改为artplayer播放器，优化选集界面
 // @author       Ryan_CC
 // @match        https://ddys.art/*
@@ -127,12 +127,16 @@
             container: '#artplayer',
             url: initUrl,
             autoplay: false,
+            autoSize: true,
+            autoMini: true,
             pip: true,
             setting: true,
             playbackRate: true,
+            hotkey: true,
             aspectRatio: true,
             fullscreen: true,
             fullscreenWeb: true,
+            miniProgressBar: true,
             autoPlayback: true,
             theme: '#3a8fb7',
             plugins: stored.seek ? [
